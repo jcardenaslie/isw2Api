c9d82fa4a7d77dd71d317b51d5b8f48cfbb27d4a
Joaquin Cardenas <jcardenas.lie@gmail.com>
2017-06-17 15:35:49 -0400

    commit inicial

---
+++ include/DbConnect.php
@@ -0,0 +1,39 @@
+DbConnect.php
+<?php
+ 
+/**
+ * Handling database connection
+ *
+ * @author Ravi Tamada
+ */
+class DbConnect {
+ 
+    private $conn;
+ 
+    function __construct() {        
+    }
+ 
+    /**
+     * Establishing database connection
+     * @return database connection handler
+     */
+    function connect() {
+        include_once dirname(__FILE__) . './Config.php';
+ 
+        // Connecting to mysql database
+        $this->conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+        mysqli_set_charset($this->conn,"utf8");
+        //$this->conn->exec("set names utf8");
+ 
+        // Check for database connection error
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+ 
+        // returing connection resource
+        return $this->conn;
+    }
+ 
+}
+ 
+?>
